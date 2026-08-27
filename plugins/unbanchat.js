5f287355846e58563effff8f645807ed0d034633
mysto off <khadabfest@gmail.com>
2026-08-27 02:59:14 +0100

    Update unbanchat.js

--- plugins/unbanchat.js
+++ plugins/unbanchat.js
@@ -1,14 +1,55 @@
-let handler = async (m) => {
-	// تأكد ان الشات موجود في قاعدة البيانات
-	global.db.data.chats[m.chat] = global.db.data.chats[m.chat] || {}
-	global.db.data.chats[m.chat].isBanned = false;
-	
-	await m.reply('*✅ تـم فـتـح الشـات بنـجـاح*')
-};
-handler.help = ['unbanchat'];
-handler.tags = ['owner'];
-handler.command = /^(unbanchat|فتح_شات)$/i;
-handler.owner = true;
-handler.group = true;
+// © 𝗝𝗜𝗧𝗢𝗦𝗦𝗔 𝗕𝗢𝗧 🇲🇦
+// فتح الشات
 
-export default handler;
+// ===== معلومات القناة =====
+const channelName = '𝗝𝗜𝗧𝗢𝗦𝗦𝗔 𝗕𝗢𝗧 🇲🇦'
+const CHANNEL_ID = '120363410733859643@newsletter'
+
+const newsletter = {
+    forwardingScore: 999,
+    isForwarded: true,
+    forwardedNewsletterMessageInfo: {
+        newsletterJid: CHANNEL_ID,
+        newsletterName: channelName
+    }
+}
+// ===========================
+
+let handler = async (m, { conn }) => {
+
+    // تأكد أن الشات موجود في قاعدة البيانات
+    global.db.data.chats[m.chat] =
+        global.db.data.chats[m.chat] || {}
+
+    // فتح الشات
+    global.db.data.chats[m.chat].isBanned = false
+
+    await conn.sendMessage(
+        m.chat,
+        {
+            text: '*✅ تـم فـتـح الـشـات بـنـجـاح*',
+            contextInfo: newsletter
+        },
+        {
+            quoted: m
+        }
+    )
+
+    await m.react('✅')
+}
+
+handler.help = [
+    'unbanchat'
+]
+
+handler.tags = [
+    'owner'
+]
+
+handler.command =
+    /^(unbanchat|فتح_شات)$/i
+
+handler.owner = true
+handler.group = true
+
+export default handler
